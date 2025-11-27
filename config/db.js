e22928e4f6f68eba81dc9f5a5d293ecd16ed9801
Milan Kumar Dandapat <milankumar07@outlook.com>
2025-11-27 16:19:06 +0530

    update new render

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     // Ensure your environment is configured to load the .env file 
     // where MONGO_URI is set to: 
     // mongodb+srv://milan-dev:Milan123@cluster0.0stui7v.mongodb.net/alumniDB?retryWrites=true&w=majority&appName=Cluster0
-    await mongoose.connect(process.env.CSE_MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
